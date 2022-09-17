175839bbb802977569f880fab4817fd320000f6a
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2022-09-17 10:33:36 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/layout/Navbar";
+import { Landing } from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+import Alert from "./components/layout/Alert";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Alert />
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+          </Routes>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
