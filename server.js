6542186293db5e28ccbf0d6ff01122c250da31c4
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2023-03-13 16:36:10 +0530

    Prepare for Deploy.

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init MiddleWare
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
+app.use(express.static("./client/build"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+const port = process.env.PORT || 5000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
